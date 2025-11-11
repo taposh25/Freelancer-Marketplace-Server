35952bf03f3982d4024ef1f862a78c3c7a97c6d7
Taposh Barai <taposhbarai03@gmail.com>
2025-11-11 18:07:07 +0600

    set-up-server-site

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port =process.env.PORT || 3000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res)=>{
+    res.send('Freelancer server site is running');
+})
+
+app.listen(port, () => {
+  console.log(`Freelancer server running on port ${port}`)
+})
+
+// username & password
