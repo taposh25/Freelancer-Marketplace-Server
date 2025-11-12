4695a99c4abc00d791878317ca698df89e52f524
Taposh Barai <taposhbarai03@gmail.com>
2025-11-12 13:55:57 +0600

    patch-and-accepted-task-added

--- index.js
+++ index.js
@@ -1,129 +1,21 @@
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const express = require("express");
-// const cors = require('cors');
-// const app = express();
-// const port =process.env.PORT || 3000;
-
-
-// // middleware 
-// app.use(cors());
-// app.use(express.json());
-
-
-// // username & password
-// // pass: vqmd2jD2as96piWU, username: jobsDB
-
-// const uri = "mongodb+srv://jobsDB:vqmd2jD2as96piWU@cluster0.aa4hy5v.mongodb.net/?appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-
-
-// app.get("/", (req, res)=>{
-//     res.send('Freelancer server site is running');
-// })
-
-// async function run(){
-//   try{
-//         await client.connect();
-
-//         const db = client.db('jobs_db');
-//         const jobCollection = db.collection('jobs');
-
-
-//         // get jobs
-
-//         app.get("/jobs", async(req, res)=>{
-//             const cursor = jobCollection.find().sort({price: 1}).limit(6);
-//             const result = await cursor.toArray();
-//             res.send(result);
-
-//         })
-
-//         // single jobs find
-
-//          app.get('/jobs/:id', async(req, res) =>{
-//           const id = req.params.id;
-//            const query = { _id: new ObjectId(id)}
-//            const result = await jobCollection.findOne(query)
-//            res.send(result);
-//          })
-
-
-
-//         // Post Jobs 
-//         app.post('/jobs', async(req, res) =>{
-//           const newjobs  = req.body;
-//           const result = await jobCollection.insertOne(newjobs);
-//           res.send(result);
-//         })
-
-//         // update post
-
-//        app.patch('/jobs/:id', async(req, res) =>{
-//       const id = req.params.id;
-//       const updatedJobs = req.body;
-//       const query = {_id: new ObjectId(id)}
-//       const update ={
-//         $set: {
-//           title: updatedJobs.title,
-//           postedBy: updatedJobs.postedBy
-//         }
-//       }
-//       const result = await productCollection.updateOne(query, update)
-//       res.send(result)
-
-//      })
-
-
-//         // Delete jobs
-
-//       app.delete('/jobs/:id', async (req, res) =>{
-//       const id = req.params.id;
-//       const query = {_id: new ObjectId(id)}
-//       const result = await jobCollection.deleteOne(query)
-//       res.send(result)
-
-//      })
-
-
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-//   }
-//   finally{
-
-//   }
-
-// }
-// run().catch(console.dir)
-
-// app.listen(port, () => {
-//   console.log(`Freelancer server running on port ${port}`)
-// })
-
-
-
-const express = require('express');
-const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port =process.env.PORT || 3000;
 
-// Middleware
+
+// middleware 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
+
+// username & password
+// pass: vqmd2jD2as96piWU, username: jobsDB
+
 const uri = "mongodb+srv://jobsDB:vqmd2jD2as96piWU@cluster0.aa4hy5v.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -133,21 +25,25 @@ const client = new MongoClient(uri, {
   }
 });
 
-app.get('/', (req, res) => {
-  res.send("Freelance Marketplace Server is Running ✅");
-});
 
-async function run() {
-  try {
-    await client.connect();
-    const db = client.db("freelance_marketplace_db");
-    const jobsCollection = db.collection("jobs");
-    const usersCollection = db.collection("users");
-    const acceptedTasksCollection = db.collection("acceptedTasks"); // ✅ new collection
 
-    /* ---------------------- JOB CRUD ---------------------- */
+app.get("/", (req, res)=>{
+    res.send('Freelancer server site is running');
+})
 
-    // ➕ Add Job
+async function run(){
+  try{
+        await client.connect();
+
+        const db = client.db('jobs_db');
+        const jobsCollection = db.collection('jobs');
+        const usersCollection = db.collection("users");
+        const acceptedTasksCollection = db.collection("acceptedTasks"); 
+
+
+
+
+        // ➕ Add Job
     app.post('/addJob', async (req, res) => {
       const newJob = req.body;
       const result = await jobsCollection.insertOne(newJob);
@@ -264,16 +160,77 @@ async function run() {
       res.send(result);
     });
 
-    // ✅ Connection test
-    await client.db("admin").command({ ping: 1 });
-    console.log("✅ MongoDB connected successfully!");
-  } finally {
-    // do not close connection for dev environment
-  }
-}
 
-run().catch(console.dir);
+
+        // get jobs
+
+        // app.get("/jobs", async(req, res)=>{
+        //     const cursor = jobsCollection.find().sort({price: 1}).limit(6);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+
+        // })
+
+        // single jobs find
+
+         app.get('/jobs/:id', async(req, res) =>{
+          const id = req.params.id;
+           const query = { _id: new ObjectId(id)}
+           const result = await jobsCollection.findOne(query)
+           res.send(result);
+         })
+
+
+
+        // Post Jobs 
+        app.post('/jobs', async(req, res) =>{
+          const newjobs  = req.body;
+          const result = await jobsCollection.insertOne(newjobs);
+          res.send(result);
+        })
+
+        // update post
+
+       app.patch('/jobs/:id', async(req, res) =>{
+      const id = req.params.id;
+      const updatedJobs = req.body;
+      const query = {_id: new ObjectId(id)}
+      const update ={
+        $set: {
+          title: updatedJobs.title,
+          postedBy: updatedJobs.postedBy
+        }
+      }
+      const result = await jobsCollection.updateOne(query, update)
+      res.send(result)
+
+     })
+
+
+        // Delete jobs
+
+      app.delete('/jobs/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query)
+      res.send(result)
+
+     })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.dir)
 
 app.listen(port, () => {
-  console.log(`🚀 Server is running on port: ${port}`);
-});
+  console.log(`Freelancer server running on port ${port}`)
+})
+
+
